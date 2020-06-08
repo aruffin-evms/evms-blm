a68504be986ead2c54a783885fda7d3509a24735
Anthony <ruffina@evms.edu>
2020-06-08 16:00:39 -0400

    updates

--- evms-blm.js
+++ evms-blm.js
@@ -1,13 +1,11 @@
 
 const firstName = document.getElementById('field93569054-first');
 const lastName = document.getElementById('field93569054-last');
-const email = document.getElementById('field93569057');
 const messageBox = document.getElementById('field93569061');
 const submitButton = document.getElementById('fsSubmitButton3910490');
 
 firstName.placeholder = 'First name';
 lastName.placeholder = 'Last name';
-email.placeholder = 'Email';
 messageBox.placeholder = 'Type your message';
 
 submitButton.value = "Add your message";
