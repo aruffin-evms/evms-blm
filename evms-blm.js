f6f87915abf53a0f8f51f98c8e5a19d9e121e536
Anthony <ruffina@evms.edu>
2020-06-08 09:55:07 -0400

    updated placeholder

--- evms-blm.js
+++ evms-blm.js
@@ -1,105 +1,104 @@
-/*
+// placeholder text - do not delete
 const firstName = document.getElementById('field93569054-first');
 const lastName = document.getElementById('field93569054-last');
-const email = document.getElementById('field93569057');
 const messageBox = document.getElementById('field93569061');
 const submitButton = document.getElementById('fsSubmitButton3910490');
 
+console.log(firstName)
 firstName.placeholder = 'First name';
 lastName.placeholder = 'Last name';
-email.placeholder = 'Email';
 messageBox.placeholder = 'Type your message';
 
 submitButton.value = "Add your message";
-*/
+
 //[start] - James Scott McDowell - 6/7/2020 - 1:57PM
 function sleep(auxiliaryFunction, columnNumber, element, range, wait){
-    var i = 0.00;
-    (function loop () {
-        setTimeout(function () {
-            if (i < range) {          // If i > 0, keep going
-                i = i + wait;
-                loop();       // Call the loop again, and pass it the current value of i
-            }
-            else{
-                auxiliaryFunction(columnNumber);
-            }
-        }, wait);
-    })();
+  var i = 0.00;
+  (function loop () {
+      setTimeout(function () {
+          if (i < range) {          // If i > 0, keep going
+              i = i + wait;
+              loop();       // Call the loop again, and pass it the current value of i
+          }
+          else{
+              auxiliaryFunction(columnNumber);
+          }
+      }, wait);
+  })();
 }
 function hideUnwantedElements(columnNumber) {
-    var feed = document.querySelector('.j-stacker');
-    var feedColumn = feed.children[columnNumber];
-    for(var j = 0; j < feedColumn.children.length; j++){
-        var feedCell = feedColumn.children[j];
-        var feedText = feedCell.children[1];  
-        var feedMessage = feedText.children[0];
-        var feedMessageText = feedMessage.children[3];
-        feedMessageText.innerHTML = feedMessageText.innerHTML.replace("Message: ", "");
+  var feed = document.querySelector('.j-stacker');
+  var feedColumn = feed.children[columnNumber];
+  for(var j = 0; j < feedColumn.children.length; j++){
+      var feedCell = feedColumn.children[j];
+      var feedText = feedCell.children[1];  
+      var feedMessage = feedText.children[0];
+      var feedMessageText = feedMessage.children[3];
+      feedMessageText.innerHTML = feedMessageText.innerHTML.replace("Message: ", "");
 
-        var feedMessageChildren = feedMessage.childNodes;
-        var paragraphCount = 0;
-        for(var i = 0; i < feedMessageChildren.length; i++){
-            if(feedMessageChildren[i].nodeName == "P"){
-                paragraphCount++;
-            }
-        }
-        if(paragraphCount == 4){
-            var feedImageContainer = feedMessage.children[4];
-            var feedImageLink = feedImageContainer.children[0];
-            var img = document.createElement("img");
-            img.src = feedImageLink.href;
-            feedMessage.appendChild(img);
-            feedImageContainer.innerHTML = "";
-        }
-    }
+      var feedMessageChildren = feedMessage.childNodes;
+      var paragraphCount = 0;
+      for(var i = 0; i < feedMessageChildren.length; i++){
+          if(feedMessageChildren[i].nodeName == "P"){
+              paragraphCount++;
+          }
+      }
+      if(paragraphCount == 4){
+          var feedImageContainer = feedMessage.children[4];
+          var feedImageLink = feedImageContainer.children[0];
+          var img = document.createElement("img");
+          img.src = feedImageLink.href;
+          feedMessage.appendChild(img);
+          feedImageContainer.innerHTML = "";
+      }
+  }
 }
 function test(number){
-    var jOverlay = document.body.lastChild;
-    var jOverlayContent = jOverlay.children[0];
-    var jPostOverlay = jOverlayContent.children[1];
-    var jOverlayText = jPostOverlay.children[1];
-    var feedMessage = jOverlayText.children[1];
-    var feedMessageText = feedMessage.children[3];
-    feedMessageText.innerHTML = feedMessageText.innerHTML.replace("Message: ", "");
+  var jOverlay = document.body.lastChild;
+  var jOverlayContent = jOverlay.children[0];
+  var jPostOverlay = jOverlayContent.children[1];
+  var jOverlayText = jPostOverlay.children[1];
+  var feedMessage = jOverlayText.children[1];
+  var feedMessageText = feedMessage.children[3];
+  feedMessageText.innerHTML = feedMessageText.innerHTML.replace("Message: ", "");
 
-    var feedMessageChildren = feedMessage.childNodes;
-    var paragraphCount = 0;
-    for(var i = 0; i < feedMessageChildren.length; i++){
-        if(feedMessageChildren[i].nodeName == "P"){
-            paragraphCount++;
-        }
-    }
-    if(paragraphCount == 4){
-        var feedImageContainer = feedMessage.children[4];
-        var feedImageLink = feedImageContainer.children[0];
-        var img = document.createElement("img");
-        img.src = feedImageLink.href;
-        feedMessage.appendChild(img);
-        feedImageContainer.innerHTML = "";
-    }
+  var feedMessageChildren = feedMessage.childNodes;
+  var paragraphCount = 0;
+  for(var i = 0; i < feedMessageChildren.length; i++){
+      if(feedMessageChildren[i].nodeName == "P"){
+          paragraphCount++;
+      }
+  }
+  if(paragraphCount == 4){
+      var feedImageContainer = feedMessage.children[4];
+      var feedImageLink = feedImageContainer.children[0];
+      var img = document.createElement("img");
+      img.src = feedImageLink.href;
+      feedMessage.appendChild(img);
+      feedImageContainer.innerHTML = "";
+  }
 }
 function hideUnwantedElements2(index) {
-    if(index == 0){
-        test(index);
-        //sleep(test, index, this, 10, 10);
-    }
+  if(index == 0){
+      test(index);
+      //sleep(test, index, this, 10, 10);
+  }
 }
 window.onload = function(){
-    var numberOfColumns = 3;
-    for(var i = 0; i < numberOfColumns; i++){
-        sleep(hideUnwantedElements, i, this, 100, 100);
-    }
-    var j = 0;
-    var checkExist = setInterval(function() {
-        if ($('.j-overlay').length) {
-           hideUnwantedElements2(j);
-           j += 1;
-           //clearInterval(checkExist);
-        }
-        else if($('.j-overlay').length == 0){
-            j = 0;
-        }
-     }, 100); // check every 100ms
+  var numberOfColumns = 3;
+  for(var i = 0; i < numberOfColumns; i++){
+      sleep(hideUnwantedElements, i, this, 100, 100);
+  }
+  var j = 0;
+  var checkExist = setInterval(function() {
+      if ($('.j-overlay').length) {
+         hideUnwantedElements2(j);
+         j += 1;
+         //clearInterval(checkExist);
+      }
+      else if($('.j-overlay').length == 0){
+          j = 0;
+      }
+   }, 100); // check every 100ms
 }
 //[end] - James Scott McDowell - 6/7/2020 - 1:57PM
