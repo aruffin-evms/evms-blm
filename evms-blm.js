b7694ee42eac50708ee22d4c4371c0b5aabe88f8
Anthony <ruffina@evms.edu>
2020-06-09 07:01:51 -0400

    updates to styles

--- evms-blm.js
+++ evms-blm.js
@@ -1,17 +1,4 @@
-/*
-const firstName = document.getElementById('field93569054-first');
-const lastName = document.getElementById('field93569054-last');
-const email = document.getElementById('field93569057');
-const messageBox = document.getElementById('field93569061');
-const submitButton = document.getElementById('fsSubmitButton3910490');
 
-firstName.placeholder = 'First name';
-lastName.placeholder = 'Last name';
-email.placeholder = 'Email';
-messageBox.placeholder = 'Type your message';
-
-submitButton.value = "Add your message";
-*/
 //[start] - James Scott McDowell - 6/7/2020 - 1:57PM
 var numberOfColumns = 3;
 function sleep(auxiliaryFunction, columnNumber, element, range, wait){
